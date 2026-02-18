c761c32ff910a293e4bb4e8de37e6a49dcb64437
itrodi <oyetunjiinioluwajesu@gmail.com>
2026-02-18 09:43:19 +0100

    socketprovider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // App.jsx
 import './utils/axiosConfig'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SocketProvider } from './context/SocketContext';
 import Overview from './components/Dashboard/Overview';
 import ProductPage from './components/DashboardSubPages/ProductPage';
 import TransactionDetails from './components/TransactionDetails';
@@ -42,194 +43,197 @@ import AdminAnalytics from './components/Auth/AdminAnalytics';
 import ForgotPassword from './components/Auth/ForgotPassword';
 import ResetPassword from './components/Auth/ResetPassword';
 import SupportPage from './components/SupportPage';
+
 function App() {
   return (
-    <Router>
-      <div className="App"> 
-        <Toaster position="top-right" />
-        <Routes>
-          {/* Public routes */}
-          <Route exact path="/" element={<AuthenticationPage/>}/> 
-          <Route exact path="/Login" element={<Login/>}/>   
-          <Route exact path="/AdminAnalytics" element={<AdminAnalytics/>}/>
-          <Route exact path="/forgot-password" element={<ForgotPassword/>}/>
-          <Route exact path="/reset-password" element={<ResetPassword/>}/>
-          {/* Protected routes */}
-          <Route exact path="/Overview" element={
-            <ProtectedRoute>
-              <Overview/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/ProductPage" element={
-            <ProtectedRoute>
-              <ProductPage/>
-            </ProtectedRoute>
-          }/> 
-          <Route exact path="/TransactionDetails" element={
-            <ProtectedRoute>
-              <TransactionDetails/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/AddProductPage" element={
-            <ProtectedRoute>
-              <AddProductPage/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Customers" element={
-            <ProtectedRoute>
-              <Customers/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/customer/:id" element={
-            <ProtectedRoute>
-              <SingleCustomerPage/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Orders" element={
-            <ProtectedRoute>
-              <Orders/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/ServicesPage" element={
-            <ProtectedRoute>
-              <ServicesPage/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/OrdersDialog" element={
-            <ProtectedRoute>
-              <OrdersDialog/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Bookings" element={
-            <ProtectedRoute>
-              <Bookings/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/BookingDetails" element={
-            <ProtectedRoute>
-              <BookingDetails/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Notifications" element={
-            <ProtectedRoute>
-              <NotificationPage/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Payments" element={
-            <ProtectedRoute>
-              <Payments/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Settings" element={
-            <ProtectedRoute>
-              <Settings/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Chatbot" element={
-            <ProtectedRoute>
-              <Chatbot/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Analytics" element={
-            <ProtectedRoute>
-              <Analytics/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/ManageStore" element={
-            <ProtectedRoute>
-              <ManageStore/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Services" element={
-            <ProtectedRoute>
-              <Services/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/AddServices" element={
-            <ProtectedRoute>
-              <AddServicesPage/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/EditProduct" element={
-            <ProtectedRoute>
-              <EditProductPage/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/EditService" element={
-            <ProtectedRoute>
-              <EditServices/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Messages" element={
-            <ProtectedRoute>
-              <Messages/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Chats" element={
-            <ProtectedRoute>
-              <Chatlist/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/PersonalDetails" element={
-            <ProtectedRoute>
-              <PersonalDetails/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/StoreSettings" element={
-            <ProtectedRoute>
-              <Store/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/StoreSetting" element={
-            <ProtectedRoute>
-              <StoreSettings/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Bank" element={
-            <ProtectedRoute>
-              <BankAccount/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Link" element={
-            <ProtectedRoute>
-              <LinkAccount/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Advance" element={
-            <ProtectedRoute>
-              <AdvanceSetting/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/product/:id" element={
-            <ProtectedRoute>
-              <IndividualProductPage/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/service/:id" element={
-            <ProtectedRoute>
-              <IndividualServicePage/>
-            </ProtectedRoute>
-          }/>
-          
-          {/* Onboarding routes - protected */}
-          <Route exact path="/Onboarding1" element={
-            <ProtectedRoute>
-              <Onboarding1/>
-            </ProtectedRoute>
-          }/>
-          <Route exact path="/Onboarding2" element={
-            <ProtectedRoute>
-              <Onboarding2/>
-            </ProtectedRoute>
-          }/>
-           <Route exact path="/SupportPage" element={
-            <ProtectedRoute>
-              <SupportPage/>
-            </ProtectedRoute>
-          }/>
-        </Routes>
-      </div>
-    </Router>
+    <SocketProvider>
+      <Router>
+        <div className="App"> 
+          <Toaster position="top-right" />
+          <Routes>
+            {/* Public routes */}
+            <Route exact path="/" element={<AuthenticationPage/>}/> 
+            <Route exact path="/Login" element={<Login/>}/>   
+            <Route exact path="/AdminAnalytics" element={<AdminAnalytics/>}/>
+            <Route exact path="/forgot-password" element={<ForgotPassword/>}/>
+            <Route exact path="/reset-password" element={<ResetPassword/>}/>
+            {/* Protected routes */}
+            <Route exact path="/Overview" element={
+              <ProtectedRoute>
+                <Overview/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/ProductPage" element={
+              <ProtectedRoute>
+                <ProductPage/>
+              </ProtectedRoute>
+            }/> 
+            <Route exact path="/TransactionDetails" element={
+              <ProtectedRoute>
+                <TransactionDetails/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/AddProductPage" element={
+              <ProtectedRoute>
+                <AddProductPage/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Customers" element={
+              <ProtectedRoute>
+                <Customers/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/customer/:id" element={
+              <ProtectedRoute>
+                <SingleCustomerPage/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Orders" element={
+              <ProtectedRoute>
+                <Orders/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/ServicesPage" element={
+              <ProtectedRoute>
+                <ServicesPage/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/OrdersDialog" element={
+              <ProtectedRoute>
+                <OrdersDialog/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Bookings" element={
+              <ProtectedRoute>
+                <Bookings/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/BookingDetails" element={
+              <ProtectedRoute>
+                <BookingDetails/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Notifications" element={
+              <ProtectedRoute>
+                <NotificationPage/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Payments" element={
+              <ProtectedRoute>
+                <Payments/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Settings" element={
+              <ProtectedRoute>
+                <Settings/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Chatbot" element={
+              <ProtectedRoute>
+                <Chatbot/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Analytics" element={
+              <ProtectedRoute>
+                <Analytics/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/ManageStore" element={
+              <ProtectedRoute>
+                <ManageStore/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Services" element={
+              <ProtectedRoute>
+                <Services/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/AddServices" element={
+              <ProtectedRoute>
+                <AddServicesPage/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/EditProduct" element={
+              <ProtectedRoute>
+                <EditProductPage/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/EditService" element={
+              <ProtectedRoute>
+                <EditServices/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Messages" element={
+              <ProtectedRoute>
+                <Messages/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Chats" element={
+              <ProtectedRoute>
+                <Chatlist/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/PersonalDetails" element={
+              <ProtectedRoute>
+                <PersonalDetails/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/StoreSettings" element={
+              <ProtectedRoute>
+                <Store/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/StoreSetting" element={
+              <ProtectedRoute>
+                <StoreSettings/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Bank" element={
+              <ProtectedRoute>
+                <BankAccount/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Link" element={
+              <ProtectedRoute>
+                <LinkAccount/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Advance" element={
+              <ProtectedRoute>
+                <AdvanceSetting/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/product/:id" element={
+              <ProtectedRoute>
+                <IndividualProductPage/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/service/:id" element={
+              <ProtectedRoute>
+                <IndividualServicePage/>
+              </ProtectedRoute>
+            }/>
+            
+            {/* Onboarding routes - protected */}
+            <Route exact path="/Onboarding1" element={
+              <ProtectedRoute>
+                <Onboarding1/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/Onboarding2" element={
+              <ProtectedRoute>
+                <Onboarding2/>
+              </ProtectedRoute>
+            }/>
+            <Route exact path="/SupportPage" element={
+              <ProtectedRoute>
+                <SupportPage/>
+              </ProtectedRoute>
+            }/>
+          </Routes>
+        </div>
+      </Router>
+    </SocketProvider>
   );
 }
 
